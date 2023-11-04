ba2ee8566a5b86d09cf4112796be982e71e50de8
Mohit Bharti <mohitbharti373@gmail.com>
2023-11-04 05:54:20 +0530

    connected mongodb

--- app.js
+++ app.js
@@ -1,16 +1,95 @@
-import express from "express";
-import dotenv from "dotenv";
 
 
 
-dotenv.config();
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
+const connectDB = require("./config/db");
+
+
+
+connectDB();
 
 
 const app =express();
-
-
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(session({
+  secret: "Our little secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(process.env.MONGODB_URL);
+
+const userSchema = new mongoose.Schema ({
+    email: String,
+  
+    password: String,
+    googleId: String
+  });
+
+  userSchema.plugin(passportLocalMongoose);
+  userSchema.plugin(findOrCreate);
+
+  const User = new mongoose.model("User", userSchema);
+
+  passport.use(User.createStrategy());
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+  
+  passport.use(new GoogleStrategy({
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:4000/auth/google/blockverse",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+  
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  ));
+
+
+
+
+
+
+
+  app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+
+
+
 
 
 const PORT = process.env.PORT || 4000;
