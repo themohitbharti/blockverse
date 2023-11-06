efe4ccbd6d7ba656e91e919bdc9bb93c455b1f69
Mohit Bharti <mohitbharti373@gmail.com>
2023-11-06 08:17:38 +0530

    added razorpay and email verification

--- app.js
+++ app.js
@@ -11,8 +11,11 @@ const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const findOrCreate = require('mongoose-findorcreate');
+const Razorpay = require('razorpay');
+const nodemailer = require("nodemailer");
 
 const connectDB = require("./config/db");
+const sendEmail = require("./utils/email.js");
 
 
 
@@ -26,6 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(express.static("public"));
+app.set('views','views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
@@ -43,6 +47,7 @@ mongoose.connect(process.env.MONGODB_URL);
 
 
   const userSchema = new mongoose.Schema({
+    username:String,
     leader_name: String,
     leader_email: String,
     profile_photo_url: String,
@@ -102,6 +107,7 @@ passport.use(new GoogleStrategy({
       if (!user) {
         // User doesn't exist, create a new user with the desired structure
         const newUser = new User({
+          username:profile.displayName,
           googleId: profile.id,
           email: email,
           leader_name: profile.displayName,
@@ -178,7 +184,9 @@ app.get("/auth/google/blockverse",
   app.get("/blockverse", function(req, res){
     if (req.isAuthenticated()){
         console.log(req.user);
-      res.render("blockverse");
+      res.render("blockverse", {
+        RAZORPAY_ID_KEY: process.env.RAZORPAY_ID_KEY
+      });
      
     } else {
       res.redirect("/login");
@@ -196,36 +204,112 @@ app.get("/auth/google/blockverse",
     });
   });
   
-
-
-
-
-
-app.post("/register", function (req, res) {
-    console.log(req.body);
-    const { username, leader_email, profile_photo_url, member_name, member_email, password } = req.body;
   
-    const user = new User({
-        username,
+  app.get("/payment_page", function(req, res){
+    res.render("payment_page");
+  });
+
+
+
+
+// app.post("/register", function (req, res) {
+//     console.log(req.body);
+//     const { username, leader_email, profile_photo_url, member_name, member_email, password } = req.body;
+  
+//     const user = new User({
+//         username,
+//       leader_email,
+//       profile_photo_url,
+//       member_name,
+//      member_email,
+      
+//     });
+  
+//     User.register(user, password, function (err, user) {
+//       if (err) {
+//         console.error(err);
+//         return res.status(500).json({ error: "Registration failed" });
+//       }
+//       passport.authenticate("local")(req, res, function () {
+//         res.redirect("/blockverse");
+//       });
+//     });
+//   });
+  
+  
+app.post("/register", async function (req, res) {
+  const { username, leader_email, profile_photo_url, member_name, member_email, password } = req.body;
+
+  if (!username) {
+      return res.status(400).json({ error: "Username is required" });
+  }
+
+  // Check if the username is already in use
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+      return res.status(400).json({ error: "Username is already taken" });
+  }
+
+  const user = new User({
+      username, // Set the provided username
       leader_email,
       profile_photo_url,
       member_name,
-     member_email,
-      
-    });
-  
-    User.register(user, password, function (err, user) {
+      member_email,
+  });
+
+  User.register(user, password, function (err, user) {
       if (err) {
-        console.error(err);
-        return res.status(500).json({ error: "Registration failed" });
+          console.error(err);
+          return res.status(500).json({ error: "Registration failed" });
       }
       passport.authenticate("local")(req, res, function () {
-        res.redirect("/blockverse");
+          res.redirect("/blockverse");
       });
-    });
   });
-  
-  
+});
+
+
+
+
+// app.post("/register", function (req, res) {
+//   const { username, leader_email, profile_photo_url, member_name, member_email, password } = req.body;
+
+//   if (!username || username.trim() === "") {
+//     return res.status(400).json({ error: "Username is required." });
+//   }
+
+//   // Check if the username is already in use
+//   User.findOne({ username: username }, (err, existingUser) => {
+//     if (err) {
+//       return res.status(500).json({ error: "Registration failed" });
+//     }
+
+//     if (existingUser) {
+//       return res.status(400).json({ error: "Username is already in use." });
+//     }
+
+//     const user = new User({
+//       username,
+//       leader_email,
+//       profile_photo_url,
+//       member_name,
+//       member_email,
+//     });
+
+//     User.register(user, password, function (err, registeredUser) {
+//       if (err) {
+//         console.error(err);
+//         return res.status(500).json({ error: "Registration failed" });
+//       }
+//       passport.authenticate("local")(req, res, function () {
+//         res.redirect("/blockverse");
+//       });
+//     });
+//   });
+// });
+
+
 
 
 
@@ -251,6 +335,116 @@ const PORT = process.env.PORT || 4000;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_ID_KEY,
+    key_secret: process.env.RAZORPAY_SECRET_KEY,
+})
+
+
+app.post("/order", (req,res)=>{
+    let options = {
+        amount : 2000,
+        currency: "INR",
+    };
+
+    razorpay.orders.create(options , function(err,order){
+        console.log(order);
+        res.json(order);
+    })
+})
+
+
+app.post("/payment/callback", (req, res) => {
+  const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
+
+  const paymentStatus = "transaction successful";
+
+  
+      res.json({
+          status: paymentStatus,
+          razorpay_order_id,
+          razorpay_payment_id,
+          razorpay_signature,
+      });
+ 
+});
+
+
+app.post("/sendRegistrationConfirmationEmail", (req,res)=>{
+  sendEmail({
+    email:req.user.email,
+    subject: "blockverse registration",
+    message: "your registration for blockverse is successful",
+ });
+ res.status(200).json({
+  status: "success",
+  message: "password reset link send to user email",
+ });
+})
+
+
+
+
+
+
+
+
+
+
 app.listen(PORT,()=>{
     console.log(`server started at ${PORT}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
